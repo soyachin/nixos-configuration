6d238071c1352f44fa0fcf010d9f24d5a241a81d
kris <miranda.salazar@utec.edu.pe>
2025-06-16 22:06:44 -0500

    fix: deleted unnecessary aliases for zsh

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -1,16 +1,9 @@
-{ config, pkgs, ... }:
-
 {
-
  programs.zsh = {
     enable = true;
     shellAliases = {
       update = "sudo nixos-rebuild switch --flake ~/.nixos-config#nixos";
-      nconfig = "sudoedit /etc/nixos/configuration.nix";
-      nhome = "sudoedit /etc/nixos/home/main-user.nix";
       ccat = "wl-copy <";
-      cdh = "cd /etc/nixos/home/";
-
     };
     oh-my-zsh = {
       enable = true;
