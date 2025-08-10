a3ff904c14d48b34453cc86c04d70025afd422e2
kris <miranda.salazar@utec.edu.pe>
2025-08-10 18:05:00 -0500

    feat: change alias to update syst.

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -2,7 +2,7 @@
  programs.zsh = {
     enable = true;
     shellAliases = {
-      update = "sudo nixos-rebuild switch --flake ~/.nixos-config#nixos";
+      update = "sudo nixos-rebuild switch --flake ~/.config/nixos/#nixos";
       ccat = "wl-copy <";
     };
     oh-my-zsh = {
