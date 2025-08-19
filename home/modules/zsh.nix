53a8eb832a68cf24fd914c3a63dde27de72915e4
kris <miranda.salazar@utec.edu.pe>
2025-08-19 13:53:09 -0500

    feat: add more aliases

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -4,6 +4,9 @@
     shellAliases = {
       update = "sudo nixos-rebuild switch --flake ~/.config/nixos/#nixos";
       ccat = "wl-copy <";
+      maicra = "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia prismlauncher"; 
+      config = "cd ~/.config/nixos/";
+
     };
     oh-my-zsh = {
       enable = true;
