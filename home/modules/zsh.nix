949bbe6ea64f1a1a68aa2567737efe7a8bcef51a
kris <miranda.salazar@utec.edu.pe>
2025-09-11 21:30:01 -0500

    chore: change update alias

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -2,7 +2,7 @@
  programs.zsh = {
     enable = true;
     shellAliases = {
-      update = "sudo nixos-rebuild switch --flake ~/.config/nixos/#nixos";
+      update = "sudo nixos-rebuild switch --flake ~/.config/nixos/#asus";
       ccat = "wl-copy <";
       maicra = "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia prismlauncher"; 
       config = "cd ~/.config/nixos/";
