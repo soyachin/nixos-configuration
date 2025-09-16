2c51dc881fc1db9f504ab772a110e563e3396820
kris <miranda.salazar@utec.edu.pe>
2025-09-16 12:06:57 -0500

    chore: add aliases for zsh

--- home/modules/zsh.nix
+++ home/modules/zsh.nix
@@ -6,7 +6,7 @@
       ccat = "wl-copy <";
       maicra = "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia prismlauncher"; 
       config = "cd ~/.config/nixos/";
-
+      gs = "git status";
     };
     oh-my-zsh = {
       enable = true;
