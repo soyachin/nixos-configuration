998fcb2813974a2666e1d51e7766109d5e6060ac
kris <miranda.salazar@utec.edu.pe>
2025-09-20 22:45:42 -0500

    fix: fix swayidle (path option doesnt exist and all)

--- home/modules/swayidle.nix
+++ home/modules/swayidle.nix
@@ -5,35 +5,33 @@
 }: {
   services.swayidle = {
     enable = true;
-    path = with pkgs; [
-      swaylock
-      sway
-      brightnessctl
+    timeouts = [
+      # dimm
+      {
+        timeout = 300; # 5 minutes
+        # -s: Saves the current brightness before dimming
+        # set 15%: Dims the screen to 15%
+        command = "brightnessctl -s set 15%";
+        # -r: Restores the saved brightness when you resume
+        resumeCommand = "brightnessctl -r";
+      }
+      # lock
+      {
+        timeout = 600; # 10 minutes
+        command = "swaylock -f -c 000000"; # Locks the screen
+      }
+      # screen off timeout
+      {
+        timeout = 630; # 10 minutes and 30 seconds
+        command = "swaymsg \"output * dpms off\""; # Turns off the display
+        resumeCommand = "swaymsg \"output * dpms on\""; # Turns on the display when returning
+      }
     ];
-    settings = {
-      timeouts = [
-        # dimm
-        {
-          timeout = 300; # 5 minutes
-          # -s: Saves the current brightness before dimming
-          # set 15%: Dims the screen to 15%
-          command = "brightnessctl -s set 15%";
-          # -r: Restores the saved brightness when you resume
-          resumeCommand = "brightnessctl -r";
-        }
-        # lock
-        {
-          timeout = 600; # 10 minutes
-          command = "swaylock -f -c 000000"; # Locks the screen
-        }
-        # screen off timeout
-        {
-          timeout = 630; # 10 minutes and 30 seconds
-          command = "swaymsg \"output * dpms off\""; # Turns off the display
-          resumeCommand = "swaymsg \"output * dpms on\""; # Turns on the display when returning
-        }
-      ];
-      before-sleep = "swaylock -f -c 000000"; # Locks the screen before suspending
-    };
+    events = [
+      {
+        event = "before-sleep";
+        command = "swaylock -f -c 000000";
+      }
+    ];
   };
 }
