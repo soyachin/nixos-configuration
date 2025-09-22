5cde5c177426d8cba9ced98e84963e319777db95
kris <miranda.salazar@utec.edu.pe>
2025-09-21 21:19:02 -0500

    feat: add cava configurations

--- home/modules/cava.nix
+++ home/modules/cava.nix
@@ -1,15 +1,38 @@
- {config, pkgs, ...}:
- 
- {
- 
-  programs.cava = {
-      enable = true;
-      settings = {
-        input = {
-          method = "fifo";
-          source = "/tmp/mpd.fifo";
-        };
-      };
-    };
 
- }
+{ config, pkgs, ... }:
+
+let
+  cavaSystemConfig = pkgs.writeTextFile {
+    name = "cava-system-config";
+    text = ''
+      [input]
+      method = "pulse"
+      source = "auto"
+    '';
+  };
+
+  cavaMpdConfig = pkgs.writeTextFile {
+    name = "cava-mpd-config";
+    text = ''
+      [input]
+      method = "fifo"
+      source = "/tmp/mpd.fifo"
+    '';
+  };
+in
+{
+  programs.cava = {
+    enable = true;
+  };
+  
+
+  home.packages = [
+    (pkgs.writeShellScriptBin "cava-system" ''
+      ${pkgs.cava}/bin/cava -p ${cavaSystemConfig}
+    '')
+    (pkgs.writeShellScriptBin "cava-mpd" ''
+      ${pkgs.cava}/bin/cava -p ${cavaMpdConfig}
+    '')
+  ];
+}
+
