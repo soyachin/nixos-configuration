28a8ef0ddb2f0e76616f1307b330e3a649d95766
kris <miranda.salazar@utec.edu.pe>
2025-09-29 23:57:03 -0500

    feat: use sddm

--- modules/services.nix
+++ modules/services.nix
@@ -1,14 +1,9 @@
-{config, pkgs, ... }:
-
 {
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = false;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  config,
+  pkgs,
+  ...
+}: {
+  services.displayManager.sddm.wayland.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -38,6 +33,5 @@
 
   services.tailscale.enable = true;
 
-  services.xserver.videoDrivers = [ "nvidia" ];
-
+  services.xserver.videoDrivers = ["nvidia"];
 }
