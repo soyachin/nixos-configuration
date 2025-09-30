ddac0653337c7331c8116ce239d4c7623d94168a
kris <miranda.salazar@utec.edu.pe>
2025-09-30 14:10:41 -0500

    refactor: refactor file

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -1,16 +1,33 @@
-{config, pkgs, ...}:
-
 {
-    fonts.packages = with pkgs; [
-    noto-fonts noto-fonts-cjk-sans noto-fonts-emoji liberation_ttf
-    aileron fragment-mono comic-mono work-sans hubot-sans eurofurence
-    dosis manrope montserrat helvetica-neue-lt-std
+  config,
+  pkgs,
+  ...
+}: {
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    liberation_ttf
+    aileron
+    fragment-mono
+    comic-mono
+    work-sans
+    hubot-sans
+    eurofurence
+    dosis
+    manrope
+    montserrat
+    helvetica-neue-lt-std
     nerd-fonts.fira-code
     nerd-fonts.ubuntu
     nerd-fonts.hack
     nerd-fonts.comic-shanns-mono
 
-    carlito dejavu_fonts ipafont kochi-substitute source-code-pro
+    carlito
+    dejavu_fonts
+    ipafont
+    kochi-substitute
+    source-code-pro
     ttf_bitstream_vera
   ];
 }
