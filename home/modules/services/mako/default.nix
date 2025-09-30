613dcc055136dbed2ebd290012af31f694adf8e5
kris <miranda.salazar@utec.edu.pe>
2025-09-30 14:10:20 -0500

    feat: customize mako

--- home/modules/services/mako/default.nix
+++ home/modules/services/mako/default.nix
@@ -6,9 +6,31 @@
   services.mako = {
     enable = true;
     settings = {
+      # gruvbox
+      background-color = "#282828";
+      text-color = "#ebdbb2";
+      border-color = "#98971a";
+      progress-color = "#b8bb26";
+
+      border-size = 2;
+      border-radius = 5;
+      default-timeout = 5000;
       font = "ComicShannsMono Nerd Font Mono 12";
-      default-timeout = 3000;
-      border-radius = 3;
+
+      anchor = "top-right";
+      margin = "30,30,10,10";
+
+      "urgency=high" = {
+        background-color = "#cc241d";
+        border-color = "#fb4934";
+        default-timeout = 0;
+      };
+
+      "urgency=low" = {
+        background-color = "#3c3836";
+        text-color = "#a89984";
+        border-color = "#665c54";
+      };
     };
   };
 }
