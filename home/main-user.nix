0c3563774299a7b901b8e84ab9bb278209f8c641
kris <miranda.salazar@utec.edu.pe>
2025-09-29 23:56:15 -0500

    mega refactor

--- home/main-user.nix
+++ home/main-user.nix
@@ -4,14 +4,7 @@
   inputs,
   unstable,
   ...
-}: let
-  volume-up = pkgs.callPackage ./modules/scripts/volume-up.nix {};
-  volume-down = pkgs.callPackage ./modules/scripts/volume-down.nix {};
-  volume-toggle = pkgs.callPackage ./modules/scripts/volume-toggle.nix {};
-  brightness-up = pkgs.callPackage ./modules/scripts/brightness-up.nix {};
-  brightness-down = pkgs.callPackage ./modules/scripts/brightness-down.nix {};
-  mic-toggle = pkgs.callPackage ./modules/scripts/mic-toggle.nix {};
-in {
+}: {
   home.username = "hojas";
   home.homeDirectory = "/home/hojas";
   home.stateVersion = "25.05";
@@ -19,10 +12,7 @@ in {
   imports = [
     ./modules
     inputs.sops-nix.homeManagerModules.sops
-    inputs.nyaa.homeManagerModule
   ];
-
-  programs.nyaa.enable = true;
 
   services.batsignal.enable = true;
 
@@ -50,26 +40,23 @@ in {
     zoom-us
     sops
     age
-    mopidy
-    ncmpcpp
-    lazygit
 
     spotify
     unstable.librewolf
-
-    swayidle
-
-    volume-up
-    volume-down
-    volume-toggle
-    brightness-up
-    brightness-down
-    mic-toggle
+    gruvbox-gtk-theme
   ];
 
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+  };
+
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.apple-cursor;
+    name = "macOS";
+    size = 32;
   };
 
   # programs.waybar.enable = true;
