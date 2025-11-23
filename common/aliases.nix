22c3e707af3e0189a7e629d75bf6b9d9795373e6
kris <miranda.salazar@utec.edu.pe>
2025-11-22 22:39:07 -0500

    feat: add common aliases for tha machines

---
+++ common/aliases.nix
@@ -0,0 +1,21 @@
+{config, pkgs, ...}:
+
+{
+  environment.shellAliases = {
+    gs = "git status";
+    gd = "git diff";
+    gcl = "git clone";
+    ga = "git add";
+    ".." = "cd ..";
+    ll = "lsd -l";
+    la = "lsd -la";
+    ls = "lsd";
+    "_" = "sudo";
+    gcm = "git commit -m";
+    gp = "git push";
+    update = "sudo nixos-rebuild switch --flake ~/.config/nixos/#$(hostname)";
+    test = "sudo nixos-rebuild test --flake ~/.config/nixos/#$(hostname)";
+    grep = "grep --color=auto";
+    config = "cd ~/.config/nixos/";
+  };
+}
