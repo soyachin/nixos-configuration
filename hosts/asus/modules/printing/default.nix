ae2efad6f1abdbd31d8c6736552e376793376931
kris <miranda.salazar@utec.edu.pe>
2026-03-26 14:14:11 -0500

    feat: add printing service

--- hosts/asus/modules/printing/default.nix
+++ hosts/asus/modules/printing/default.nix
@@ -1,16 +1,28 @@
 { pkgs, ... }:
 {
-  services.avahi = {
+services.avahi = {
+  enable = true;
+  nssmdns4 = true;
+  openFirewall = true;
+  publish = {
     enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
+    userServices = true;
   };
+};
 
-  services.printing = {
-    enable = true;
-    drivers = with pkgs; [
-      cups-filters
-      cups-browsed
-    ];
-  };
+services.printing = {
+  enable = true;
+  browsing = true; 
+  browsedConf = ''
+    BrowseDNSSDSubTypes _cups,_print
+    BrowseLocalProtocols all
+    BrowseRemoteProtocols all
+    CreateIPPPrinterQueues All
+    BrowseProtocols all
+  '';
+  drivers = with pkgs; [ 
+    cups-filters
+    gutenprint 
+  ];
+};
 }
