4c81ce211941a6b36fc45086a5ced9a37ed04b9f
kris <miranda.salazar@utec.edu.pe>
2026-03-26 14:13:55 -0500

    feat: add cups printing port

--- hosts/asus/modules/network/default.nix
+++ hosts/asus/modules/network/default.nix
@@ -1,12 +1,10 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
   networking.firewall = {
-    allowedTCPPorts = [22 22220];
-    allowedUDPPortRanges = [
-    { from = 49152; to = 65535; }
-  ];
+    allowedTCPPorts = [
+      22
+      22220
+      5353
+    ];
+    allowedUDPPorts = [ 631 ];
   };
 }
