b375721298899e5d96163338bdb808148a3e2df2
kris <miranda.salazar@utec.edu.pe>
2026-04-08 15:27:19 -0500

    feat: tratar de usar positron

--- hosts/asus/programs/positron/default.nix
+++ hosts/asus/programs/positron/default.nix
@@ -1,39 +1,35 @@
 { pkgs, unstable, lib, ... }:
 
 let
-  # 1. Entorno de R con tus librerías
+  # Definimos el entorno de R como una variable que podamos reusar
   my-r-env = unstable.rWrapper.override {
     packages = with unstable.rPackages; [
       dplyr readr janitor ggplot2 jsonlite scales GGally 
-      rvest xml2 XML tidyr DescTools
+      rvest xml2 XML tidyr DescTools DBI RSQLite
     ];
   };
 
-  # 2. Entorno de Python con tus librerías
   my-python-env = unstable.python3.withPackages (ps: with ps; [
     numpy matplotlib jupyterlab scikit-learn notebook
-    networkx ipykernel nbconvert nbdime pandoc sympy
-    graphviz pandas
+    networkx ipykernel nbconvert nbdime sympy
+    graphviz pandas pypandoc
   ]);
 
-  # 3. El Wrapper de Positron
   positron-custom = unstable.symlinkJoin {
     name = "positron";
     paths = [ unstable.positron-bin ];
     nativeBuildInputs = [ pkgs.makeWrapper ];
     postBuild = ''
       wrapProgram $out/bin/positron \
-        --prefix PATH : ${lib.makeBinPath [ my-r-env my-python-env ]}
+        --prefix PATH : ${lib.makeBinPath [ my-r-env my-python-env unstable.pandoc ]} \
+        --set R_HOME ${my-r-env}/lib/R
     '';
   };
 in
 {
   environment.systemPackages = [
-    positron-custom
-  ];
-
-  # Opcional: Asegurar el permiso unfree específicamente para este binario
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
-    "positron-bin"
+    positron-custom  # El IDE envuelto
+    my-r-env         # <--- ESTO hará que 'which R' funcione en tu terminal
+    unstable.pandoc  # También útil tenerlo a mano
   ];
 }
