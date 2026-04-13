bd2038343d77f847335c64791aaac0d226f9afb8
kris <miranda.salazar@utec.edu.pe>
2026-04-12 20:15:33 -0500

    fix: corregir sshkey de termux

--- common/ssh-keys.nix
+++ common/ssh-keys.nix
@@ -5,5 +5,5 @@
   aobaMini = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIUOyi3z2auIjp6c+zPdQjhCtU/wSaM8z2ia7u6sFe4x aoba@mini";
 
   # TODO: Añadir clave de Termux
-  termux = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHVJUBVO4cjW4C8LgYNlEMXBvlmvfNj7c6NBecG9L7E9 termux";
+  termux = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICaXZ8aScokknEP7j8IfrZcWq/PEdzItzP9acAODjJ0L termux";
 }
