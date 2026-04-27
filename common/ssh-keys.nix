9146999a99b6487d227336908bdf15d9998ab892
kris <miranda.salazar@utec.edu.pe>
2026-04-27 11:53:56 -0500

    feat(common): configure ssh key for GH actions

--- common/ssh-keys.nix
+++ common/ssh-keys.nix
@@ -6,4 +6,7 @@
 
   # TODO: Añadir clave de Termux
   termux = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICaXZ8aScokknEP7j8IfrZcWq/PEdzItzP9acAODjJ0L termux";
+
+  # github actions
+  githubActions = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF1iYWiey3GBoh6iA4ORsy8dzE4YvqJjVnTdlcOoJQzm github-actions-deploy";
 }
