d8c365289c478abc62badacca731caced3bd1930
kris <miranda.salazar@utec.edu.pe>
2026-05-05 08:45:42 -0500

    fix: fix bluetooth and mic volume issues

--- common/desktop.nix
+++ common/desktop.nix
@@ -1,20 +1,55 @@
-{ ... }: {
+{ pkgs, ... }:
+{
   boot.supportedFilesystems = [ "ntfs" ];
-
   services.printing.enable = true;
   services.udisks2.enable = true;
   services.blueman.enable = true;
-
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
-    alsa = { enable = true; support32Bit = true; };
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
     pulse.enable = true;
     audio.enable = true;
-    wireplumber.enable = true;
+    wireplumber = {
+      enable = true;
+      extraConfig."51-mic-volume" = {
+        "monitor.alsa.rules" = [
+          {
+            matches = [ { "node.name" = "alsa_input.pci-0000_06_00.6.analog-stereo"; } ];
+            actions.update-props = {
+              "node.volume" = 0.20;
+              "volume.limit" = 0.20;
+            };
+          }
+        ];
+      };
+    };
   };
-
   hardware.bluetooth = {
     enable = true;
+  };
+  systemd.user.services.mic-volume = {
+    description = "Keep mic at 20%";
+    wantedBy = [ "default.target" ];
+    after = [ "wireplumber.service" ];
+    path = [
+      pkgs.wireplumber
+      pkgs.pipewire
+    ];
+    serviceConfig = {
+      Type = "simple";
+      Restart = "always";
+      ExecStart = pkgs.writeShellScript "mic-volume-watch" ''
+        sleep 2
+        wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 0.2
+        pactl subscribe | grep --line-buffered "source" | while read -r _; do
+          wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 0.2
+          wpctl set-volume 68 0.2
+        done
+      '';
+    };
   };
 }
--- hosts/asus/modules/hardware/bluetooth.nix
+++ hosts/asus/modules/hardware/bluetooth.nix
@@ -1,4 +1,21 @@
-{ ... }: {
+{ ... }:
+{
   hardware.enableRedistributableFirmware = true;
   boot.kernelModules = [ "btusb" ];
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+
+  # Udev rules para DS4
+  services.udev.extraRules = ''
+    KERNEL=="hidraw*", KERNELS=="*054C:05C4*", MODE="0660", TAG+="uaccess"
+    KERNEL=="hidraw*", KERNELS=="*054C:09CC*", MODE="0660", TAG+="uaccess"
+  '';
+
+  # Grupos del usuario
+  users.users.hojas.extraGroups = [
+    "input"
+    "bluetooth"
+  ];
 }
