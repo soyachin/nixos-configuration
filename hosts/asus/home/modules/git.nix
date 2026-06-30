8753af0896ad3dbe18f85b97982ccfee3a74a355
kris <miranda.salazar@utec.edu.pe>
2026-06-30 12:15:38 -0500

    feat: config ssh correctly

--- hosts/asus/home/modules/git.nix
+++ hosts/asus/home/modules/git.nix
@@ -11,6 +11,33 @@
 
   programs.ssh = {
     enable = true;
-    addKeysToAgent = "yes";
+    enableDefaultConfig = false;
+
+    settings = {
+      "*" = {
+        AddKeysToAgent = "yes";
+        Compression = "no";
+        ControlMaster = "no";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "no";
+        ForwardAgent = "no";
+        HashKnownHosts = "no";
+        ServerAliveCountMax = 3;
+        ServerAliveInterval = 0;
+        UserKnownHostsFile = "~/.ssh/known_hosts";
+      };
+
+      "github.com" = {
+        IdentityFile = "~/.ssh/github_edu_id_ed25519";
+        IdentitiesOnly = "yes";
+      };
+
+      "mini" = {
+        HostName = "100.108.151.5";
+        User = "aoba";
+        IdentityFile = "~/.ssh/id_ed25519";
+        IdentitiesOnly = "yes";
+      };
+    };
   };
 }
